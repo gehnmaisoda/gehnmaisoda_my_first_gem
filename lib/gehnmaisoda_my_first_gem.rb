69afe869b1f76808b498d19728e08befbe143eb4
shingo maeda <gehnmaisoda@gmail.com>
2024-05-01 21:45:56 +0900

    initial commit

---
+++ lib/gehnmaisoda_my_first_gem.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "gehnmaisoda_my_first_gem/version"
+
+module GehnmaisodaMyFirstGem
+  class Error < StandardError; end
+  # Your code goes here...
+end
